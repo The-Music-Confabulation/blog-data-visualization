56adae40099a4d6a682d3cf411765c45a1a5a89e
Thanh Nam Tran <ttran19@umbc.edu>
2022-03-11 01:37:34 -0500

    export data to json save as collection.json

--- data_generator.py
+++ data_generator.py
@@ -18,20 +18,29 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 import os
 from dotenv import load_dotenv
+from bson.json_util import dumps
 load_dotenv()
-
+import certifi
+import json
+ca = certifi.where()
 
 config = dotenv_values(".env")
 
+client = pymongo.MongoClient(f"mongodb+srv://" + config['SERVER_USR'] + ":" + config['SERVER_PWD'] +
+                             "@cluster0.1wmqh.mongodb.net/blog_content"
+                             "?retryWrites=true&w=majority", tlsCAFile=ca)
+
+db = client.blog_content
+collection = db['posts']
+
+cursor = collection.find({})
+with open('collection.json', 'w') as file:
+    file.write(dumps(cursor))
 
 
-username = config['USERNAME'] + '@' + config['DOMAIN']
-password = config['PASSWORD']
+with open('collection.json', 'r') as myfile:
+    data=myfile.read()
 
-print(username)
+obj = json.loads(data)
 
-chrome_driver_path = "/Users/ThanhNam/PycharmProjects/yt-manager/chromedriver.exe"
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://www.youtube.com/")
-# print(password)
