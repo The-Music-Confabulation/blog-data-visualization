5ea70a4c9eb8dc3e3ada1e6201eb8e66e575876e
Thanh Nam Tran <ttran19@umbc.edu>
2022-03-11 05:28:25 -0500

    install pandas, set up file

--- data_generator.py
+++ data_generator.py
@@ -1,58 +1,34 @@
-from dotenv import load_dotenv
 from dotenv import dotenv_values
 import pymongo
 import os
 from pymongo import MongoClient
-import pprint
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
 import os
 from dotenv import load_dotenv
 from bson.json_util import dumps
 import time
 import certifi
 import json
+
 load_dotenv()
 
 
-url_list = []
-
-
-def getURL():
+def getData():
     ca = certifi.where()
-
     config = dotenv_values(".env")
-
     client = pymongo.MongoClient(f"mongodb+srv://" + config['SERVER_USR'] + ":" + config['SERVER_PWD'] +
                                  "@cluster0.1wmqh.mongodb.net/blog_content"
                                  "?retryWrites=true&w=majority", tlsCAFile=ca)
-
     db = client.blog_content
     collection = db['posts']
-
     cursor = collection.find({})
 
     if os.path.exists("collection.json"):
-
         # if file exists, read and extract length
         with open('collection.json', 'r') as myfile:
             data = myfile.read()
         obj = json.loads(data)
 
-        old_num = len(obj)
-        print(obj)
         # Delete old json
-        print("Delete and create new json")
         os.remove("collection.json")
 
         # Export new Json,
@@ -60,28 +36,12 @@ def getURL():
             file.write(dumps(cursor))
 
         time.sleep(5)
-
         # Read new Json data,
         with open('collection.json', 'r') as myfile:
             data = myfile.read()
         obj = json.loads(data)
-        print(obj)
-        # This is the current number of posts
-        new_num = len(obj)
 
-        if old_num == new_num:
-            return None
-        else:
-            # 0 1
-            # 0 1 2 3
-            # get element at 2 3
-            print("Getting the url \n")
-            for i in range(old_num, new_num):
-                url_list.append(str(obj[i]['url']))  # this is the url
-
-            print(url_list)
-            print("Done \n")
-            return url_list
+        return obj
     else:
         # create new data.json file
         print("Data does not exist. Create new Json file")
@@ -95,7 +55,4 @@ def getURL():
             data = myfile.read()
         obj = json.loads(data)
 
-        for i in range(len(obj)):
-            url_list.append(str(obj[i]['url']))
-        return url_list
-
+        return obj
--- main.py
+++ main.py
@@ -1,49 +1,9 @@
-from dotenv import load_dotenv
-from dotenv import dotenv_values
-import pymongo
-import os
-from pymongo import MongoClient
-import pprint
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-import os
-from dotenv import load_dotenv
-import json
-from data_generator import getURL
-load_dotenv()
+from data_generator import getData
+import pandas as pd
 
-# Get data from mongodb server and export to JSon
-# urls = getURL()
-urls = 'BUrv5IhE3Og'
-if urls is None:
-    # There is nothing to add
-    pass
-else:
-    config = dotenv_values(".env")
-    username = config['USERNAME'] + '@' + config['DOMAIN']
-    password = config['PASSWORD']
+with open('collection.json', encoding='utf-8') as inputfile:
+    df = pd.read_json(inputfile)
 
-    print(urls)
-    print(username)
-    print(password)
+df.to_csv('csvfile.csv', encoding='utf-8', index=False)
 
-    ser = Service("/Users/ThanhNam/PycharmProjects/yt-manager/chromedriver.exe")
-    op = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=ser, options=op)
-
-    driver.get("https://www.youtube.com/")
-
-
-
-    #driver.close()
+print(getData())
