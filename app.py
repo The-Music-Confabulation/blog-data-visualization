caf3612878be88b8a06deaae9e6761835e51e632
Thanh Nam Tran <ttran19@umbc.edu>
2022-03-12 22:12:51 -0500

    bar graph

--- app.py
+++ app.py
@@ -20,16 +20,12 @@ import pandas as pd
 from dash import Dash, dcc, html, Input, Output
 import os
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+app = Dash(__name__)
 
 server = app.server
 
-
 with open('collection.json', encoding='utf-8') as inputfile:
     df = pd.read_json(inputfile)
-
 
 df['total_like'] = df['like'].cumsum()
 
@@ -39,51 +35,50 @@ df['total_like'] = df['like'].cumsum()
 fig = px.line(df, x="date", y="like", title='Number of likes over time')
 
 fig.update_layout(
-    autosize=False,
-    width=500,
-    height=500,
-    margin=dict(
-        l=50,
-        r=50,
-        b=100,
-        t=100,
-        pad=4
-    ),
     paper_bgcolor="LightSteelBlue",
+    title_x=0.5
 )
+
+fig.update_traces(mode="markers+lines")
+
 df2 = df.groupby('date', as_index=False)['_id'].count()
 
 fig_2 = px.line(df2, x="date", y="_id", title='Number of posts over time')
 fig_2.update_layout(
-    autosize=False,
-    width=500,
-    height=500,
-    margin=dict(
-        l=50,
-        r=50,
-        b=100,
-        t=100,
-        pad=4
-    ),
     paper_bgcolor="LightSteelBlue",
+    title_x=0.5
 )
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
 
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
+fig_2.update_traces(mode="markers+lines")
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    ),
-    dcc.Graph(
-        id='example-graph-2',
-        figure=fig_2
-    )
-])
+fig_3 = px.bar(df, x="title", y="like", title='Posts and likes')
+fig_3.update_layout(
+    paper_bgcolor="LightSteelBlue",
+    title_x=0.5
+)
 
+
+app.layout = html.Div(
+    children=[
+
+        html.H1(children=['Welcome to the Music Confabulation - Data Visualization'], style={'textAlign': 'center'}),
+
+        html.Div(children=dcc.Graph(
+            id='example-graph',
+            figure=fig
+        ), style={'margin': '20px'})
+        ,
+        html.Div(children=dcc.Graph(
+                id='example-graph-2',
+                figure=fig_2
+        ), style={'margin': '20px'}),
+
+        html.Div(children=dcc.Graph(
+                id='example-graph-3',
+                figure=fig_3
+        ), style={'margin': '20px'})
+
+    ], style={'margin': 'auto', 'border': '2px solid black'})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
