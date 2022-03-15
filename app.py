dd4e08813fbaf40e8807331ce8f34fd2dd2418dc
Thanh Nam Tran <ttran19@umbc.edu>
2022-03-15 00:00:37 -0400

    daily build

--- app.py
+++ app.py
@@ -1,18 +1,4 @@
 
-import pandas as pd
-import plotly.express as px
-from dash import Dash, html, dcc
-import plotly.express as px
-import pandas as pd
-
-# print(getData())
-
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 from dash import Dash, dcc, html
 import plotly.express as px
 import pandas as pd
