d6a62a0a8260b5ef93925c616eb08d64c1c4b2da
Thanh Nam Tran <ttran19@umbc.edu>
2022-03-20 16:00:23 -0400

    update data

--- app.py
+++ app.py
@@ -1,67 +1,118 @@
-
 from dash import Dash, dcc, html
 import plotly.express as px
 import pandas as pd
-
 from dash import Dash, dcc, html, Input, Output
 import os
+import plotly.io as pio
 
 app = Dash(__name__)
 
 server = app.server
 
-with open('collection.json', encoding='utf-8') as inputfile:
+with open('post.json', encoding='utf-8') as inputfile:
     df = pd.read_json(inputfile)
 
-df['total_like'] = df['like'].cumsum()
-
-fig = px.line(df, x="date", y="like", title='Number of likes over time')
-
-fig.update_layout(
-    paper_bgcolor="LightSteelBlue",
-    title_x=0.5
+# --------------Number of posts over time------------------#
+df_pot = df.groupby('date', as_index=False)['title'].count()
+df_pot['title'].astype(int)
+fig_1 = px.line(df_pot, x="date", y="title", title='Number of posts over time')
+fig_1.update_layout(
+    title_x=0.5,
+    paper_bgcolor="aliceblue",
+    yaxis=dict(
+        tickmode='linear'
+    )
 )
 
-fig.update_traces(mode="markers+lines")
+fig_1.update_traces(mode="markers+lines")
 
-df2 = df.groupby('date', as_index=False)['_id'].count()
+# --------------Number of interactions over time------------------#
+df['total_like'] = df.apply(lambda row: len(row['like']), axis=1)
+df['total_cmt'] = df.apply(lambda row: len(row['comments']), axis=1)
 
-fig_2 = px.line(df2, x="date", y="_id", title='Number of posts over time')
+df_int = df.groupby('date', as_index=False)['total_like'].sum()
+df_int2 = df.groupby('date', as_index=False)['total_cmt'].sum()
+
+fig_2 = px.line(df_int, x="date", y="total_like", title='Number of likes over time')
 fig_2.update_layout(
-    paper_bgcolor="LightSteelBlue",
-    title_x=0.5
+    title_x=0.5,
+    paper_bgcolor="aliceblue",
+    yaxis=dict(
+        tickmode='linear'
+    )
 )
-
 fig_2.update_traces(mode="markers+lines")
 
-fig_3 = px.bar(df, x="title", y="like", title='Posts and likes')
+fig_3 = px.line(df_int2, x="date", y="total_cmt", title='Number of comments over time')
 fig_3.update_layout(
-    paper_bgcolor="LightSteelBlue",
-    title_x=0.5
+    title_x=0.5,
+    paper_bgcolor="aliceblue",
+    yaxis=dict(
+        tickmode='linear'
+    )
+)
+fig_3.update_traces(mode="markers+lines")
+
+# --------------Rank of post by likes------------------#
+
+fig_4 = px.bar(df, x="title", y="total_like", title='Rank of post by likes')
+fig_4.update_layout(
+    title_x=0.5,
+    paper_bgcolor="aliceblue",
+    yaxis=dict(
+        tickmode='linear'
+    )
 )
 
+# --------------Set Up User Schema------------------#
+with open('user.json', encoding='utf-8') as inputfile:
+    df_user = pd.read_json(inputfile)
 
+# --------------Current user over time------------------#
+df_user['dateJoin'] = pd.to_datetime(df_user['dateJoin'])
+df_uot = df_user.groupby('dateJoin', as_index=False)['username'].count()
+
+fig_5 = px.line(df_uot, x="dateJoin", y="username", title='Rank of post by likes')
+fig_5.update_layout(
+    title_x=0.5,
+    paper_bgcolor="aliceblue",
+    yaxis=dict(
+        tickmode='linear'
+    )
+)
+fig_5.update_traces(mode="markers+lines")
+# --------------Set Up Dashboard------------------#
 app.layout = html.Div(
     children=[
 
         html.H1(children=['Welcome to the Music Confabulation - Data Visualization'], style={'textAlign': 'center'}),
 
         html.Div(children=dcc.Graph(
-            id='example-graph',
-            figure=fig
-        ), style={'margin': '20px'})
-        ,
-        html.Div(children=dcc.Graph(
-                id='example-graph-2',
-                figure=fig_2
-        ), style={'margin': '20px'}),
+            id='fg_1',
+            figure=fig_1
+        ), style={'margin': '20px', 'border': '2px solid black'}),
 
         html.Div(children=dcc.Graph(
-                id='example-graph-3',
-                figure=fig_3
-        ), style={'margin': '20px'})
+            id='fg_2',
+            figure=fig_2
+        ), style={'margin': '20px', 'border': '2px solid black'}),
 
-    ], style={'margin': 'auto', 'border': '2px solid black'})
+        html.Div(children=dcc.Graph(
+            id='fg_3',
+            figure=fig_3
+        ), style={'margin': '20px', 'border': '2px solid black'}),
+
+        html.Div(children=dcc.Graph(
+            id='fg_4',
+            figure=fig_4
+        ), style={'margin': '20px', 'border': '2px solid black'}),
+
+        html.Div(children=dcc.Graph(
+            id='fg_5',
+            figure=fig_5
+        ), style={'margin': '20px', 'border': '2px solid black'}),
+
+    ], style={'margin': 'auto'})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
